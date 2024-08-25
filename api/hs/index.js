3ab85d253984e43b53f63d48bf6b61a27880949b
Aisar <ahmed.aisar@gmail.com>
2024-08-25 17:53:08 +0500

    init26

--- api/hs/index.js
+++ api/hs/index.js
@@ -8,10 +8,9 @@ async function scrapeHotelData(page, checkin, checkout, adults = 2, child = 0) {
   const searchUrl = `https://hotelscan.com/en`;
 
 
-  await Promise.all([
-    page.goto(searchUrl, { waitUntil: "domcontentloaded" }),
-    page.goto(`https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ''}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z`, { waitUntil: 'networkidle2' })
-  ])
+  await page.goto(searchUrl, { waitUntil: "domcontentloaded" })
+
+  await page.goto(`https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ''}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z`, { waitUntil: 'networkidle2' })
 
   let body = await page.waitForSelector('body');
   let json = await body?.evaluate(el => el.textContent);
@@ -44,11 +43,11 @@ module.exports = async (req, res) => {
 
     const page = await browser.newPage();
 
-    const ua = await page.evaluate("navigator.userAgent");
-
-    await page.setUserAgent(ua);
+    //await page.setUserAgent(ua);
 
     await page.setRequestInterception(true);
+
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36')
 
     await page.on("request", async (request) => {
       if (
