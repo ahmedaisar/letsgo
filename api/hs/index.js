ee54c2f5051a5f680a9099d17835cd7a2b821da0
Aisar <ahmed.aisar@gmail.com>
2024-08-25 18:05:26 +0500

    init28

--- api/hs/index.js
+++ api/hs/index.js
@@ -8,7 +8,7 @@ async function scrapeHotelData(page, checkin, checkout, adults, child) {
   const searchUrl = `https://hotelscan.com/en`;
 
 
-  await page.goto(searchUrl, { waitUntil: "domcontentloaded" })
+  await page.goto(searchUrl, { waitUntil: "load" })
 
   await page.goto(`https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ''}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z`, { waitUntil: 'networkidle2' })
 
@@ -59,7 +59,7 @@ module.exports = async (req, res) => {
 
     const hotels = await scrapeHotelData(page, checkin, checkout, adults, child);
  
-    res.status(200).json(hotels);
+    res.status(200).json(JSON.stringify(hotels));
   } catch (error) {
     console.log(error);
     res.statusCode = 500;
