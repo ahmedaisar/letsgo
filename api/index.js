688064fe23fcfba0d8e08c09ab2299875b069cd5
Aisar <ahmed.aisar@gmail.com>
2024-08-26 16:14:10 +0500

    #express16

--- api/index.js
+++ api/index.js
@@ -55,7 +55,7 @@ async function scrapeHotelData(checkin, checkout, adults, child) {
     await page.goto(searchUrl, { waitUntil: "load" });
 
     await page.goto(
-      `https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ""}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
+      `https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ""}&mobile=0&loop=3&country=MV&ef=1&geoid=x5p4hmhw6iot&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0`,
       { waitUntil: "networkidle0" }
     );
 
