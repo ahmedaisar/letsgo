6c4e8c5c7df1ae7bfaf3b3b74b129f1b669bdf1e
Aisar <ahmed.aisar@gmail.com>
2024-08-26 11:18:28 +0500

    init49

--- api/hs/index.js
+++ api/hs/index.js
@@ -1,50 +1,27 @@
-const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium-min");
-
-export const maxDuration = 30;
+const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  let browser = null;
   const { checkin, checkout, adults, child } = req.query;
 
-  chromium.setHeadlessMode = true;
-
-  chromium.setGraphicsMode = false;
-
-  const executablePath = await chromium.executablePath(
-    `https://github.com/Sparticuz/chromium/releases/download/v123.0.1/chromium-v123.0.1-pack.tar`
-  );
-
   try {
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox"],
-      executablePath: executablePath,
-      headless: true,
+    const searchUrl = `https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ''}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z`;
+
+    const response = await fetch(searchUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+        'Accept': 'application/json',
+      },
     });
 
-    const page = await browser.newPage();
-    await page.setRequestInterception(true);
-    await page.on("request", (request) => {
-      if (["image", "stylesheet", "font"].includes(request.resourceType())) {
-        request.abort();
-      } else {
-        request.continue();
-      }
-    });
-
-    // Your scraping logic here
-    const searchUrl = `https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ""}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z`;
-    await page.goto(searchUrl, { waitUntil: "networkidle2" });
-
-    const json = await page.evaluate(() => document.body.textContent);
-
-    res.status(200).json(JSON.parse(json));
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error });
-  } finally {
-    if (browser !== null) {
-      await browser.close();
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    const data = await response.text();
+
+    res.status(200).json(JSON.parse(data));
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred during the request' });
   }
 };
