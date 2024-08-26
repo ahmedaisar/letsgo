27a84868d9e9c67a104f6c7b11d15ebf9bb19927
Aisar <ahmed.aisar@gmail.com>
2024-08-26 10:43:31 +0500

    init41

--- api/hs/index.js
+++ api/hs/index.js
@@ -1,6 +1,25 @@
 const puppeteer = require("puppeteer-core");
 const chrome = require("@sparticuz/chromium-min");
 
+export const maxDuration = 30
+ 
+ 
+async function scrapeHotelData(page, checkin, checkout, adults, child) {
+   // Construct the search URL
+  const searchUrl = `https://hotelscan.com/en/search?geoid=x5p4hmhw6iot&checkin=${checkin}&checkout=${checkput}&rooms=${adults}${child}`;
+
+
+  await page.goto(searchUrl, { waitUntil: "domcontentloaded" })
+
+  await page.goto(`https://hotelscan.com/combiner?pos=zz&locale=en&checkin=${checkin}&checkout=${checkout}&rooms=${adults}${child ? child : ''}&mobile=0&loop=3&country=MV&ef=1&geoid=xmmmamtksdxx&toas=hotel%2Cbed_and_breakfast%2Cguest_house%2Cresort&deviceNetwork=4g&deviceCpu=20&deviceMemory=8&limit=25&offset=0z`, { waitUntil: 'networkidle2' })
+
+  let body = await page.waitForSelector('body');
+  let json = await body?.evaluate(el => el.textContent);
+
+  return json
+
+}
+
  
 module.exports = async (req, res) => {
   let browser;
@@ -12,6 +31,7 @@ module.exports = async (req, res) => {
     );
 
     browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
       executablePath: executablePath,
       headless: true,
       ignoreHTTPSErrors: true,
@@ -44,7 +64,10 @@ module.exports = async (req, res) => {
     res.status(200).json(hotels);
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    res.statusCode = 500;
+    res.json({
+      body: "Sorry, Something went wrong!",
+    });
   } finally {
     if (browser) {
       await browser.close();
